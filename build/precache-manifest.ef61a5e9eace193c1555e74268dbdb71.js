6612fa0746e2c0bce8a50fee00cdd92fb3371c12
Trinh Le <TrinhL@hpa.org.nz>
2018-11-26 12:52:07 +1300

    added build

---
+++ build/precache-manifest.ef61a5e9eace193c1555e74268dbdb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38ae8a32107bfeed5e7b",
+    "url": "/static/js/main.38ae8a32.chunk.js"
+  },
+  {
+    "revision": "b2bc4472e79ec0d62230",
+    "url": "/static/js/1.b2bc4472.chunk.js"
+  },
+  {
+    "revision": "38ae8a32107bfeed5e7b",
+    "url": "/static/css/main.c76918e4.chunk.css"
+  },
+  {
+    "revision": "93519f08b0cf2969fba962d9da89be42",
+    "url": "/index.html"
+  }
+];
